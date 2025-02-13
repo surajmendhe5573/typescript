ba61d2f56160e1801d17cb3859992f954107d626
surajmendhe5573 <surajmendhe470@gmail.com>
2025-02-13 12:23:22 +0530

    changes

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -6,44 +6,58 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const register = async (req: Request, res: Response): Promise<void> => {
-  const { name, email, password } = req.body;
-
   try {
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
+    const { name, email, password } = req.body;
+
+    if(!name || !email || !password){
+        res.status(400).json({message: 'All fields are required'});
+        return;
+    }
+
+    const userExist = await User.findOne({ email });
+    if (userExist) {
       res.status(400).json({ message: "User already exists" });
-      return; // Add return to stop further execution
+      return; 
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user: IUser = new User({ name, email, password: hashedPassword });
-    await user.save();
+    const newUser: IUser = new User({
+      name,
+      email,
+      password: hashedPassword
+    })
+    await newUser.save();
 
-    res.status(201).json({ message: "User registered successfully" , user});
+    res.status(201).json({ message: "User registered successfully" , newUser});
   } catch (error) {
     res.status(500).json({ message: "Error registering user", error });
   }
 };
 
 export const login = async (req: Request, res: Response): Promise<void> => {
-  const { email, password } = req.body;
-
   try {
-    const user = await User.findOne({ email });
-    if (!user) {
-      res.status(400).json({ message: "Invalid email or password" });
-      return; // Add return to stop further execution
+    const { email, password } = req.body;
+
+    if(!email || !password){
+       res.status(400).json({message: 'Email and password are required'});
+       return
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+    const userExist = await User.findOne({ email });
+    if (!userExist) {
+      res.status(400).json({ message: "Invalid email or password" });
+      return; 
+    }
+
+    const isMatch = await bcrypt.compare(password, userExist.password);
     if (!isMatch) {
       res.status(400).json({ message: "Invalid email or password" });
-      return; // Add return to stop further execution
+      return; 
     }
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET || 'JWT_SECRET', { expiresIn: '1h' });
-    res.status(200).json({ token });
+    const token = jwt.sign({ id: userExist._id }, process.env.JWT_SECRET || 'JWT_SECRET', { expiresIn: '1h' });
+    res.status(200).json({message: 'User login successfully', token});
   } catch (error) {
     res.status(500).json({ message: "Error logging in", error });
   }
